f87d77ad37ded66c4265f61674db4763e520a8b2
M1778M <m1778.pc@gmail.com>
2025-12-09 23:33:41 +0330

    Renamed the leaderstats in server to player because it's more general. And refined easycollector module'

---
+++ src/server/player.server.ts
@@ -0,0 +1,31 @@
+// import ProfileStore from "@rbxts/profile-store";
+import { Workspace, Players } from "@rbxts/services";
+import { Collector } from "shared/easycollector";
+
+Collector.WrapEvent(
+	"playeradded",
+	Players.PlayerAdded.Connect(function (player: Player) {
+		Collector.WrapEvent(
+			"characteradded",
+			player.CharacterAdded.Connect(function (character) {
+				(character.WaitForChild("Humanoid") as Humanoid).Health = 50;
+				Collector.WrapEvent(
+					"baseplate_touch",
+					(Workspace.WaitForChild("Baseplate") as Part).Touched.Connect(function () {
+						print("Player `" + player.Name + "` with character" + character.Name + " Touched baseplate.");
+						(character.WaitForChild("Humanoid") as Humanoid).Health = 30;
+					}),
+				);
+			}),
+		);
+	}),
+);
+
+Collector.WrapEvent(
+	"playerremoving",
+	Players.PlayerRemoving.Connect(function (player) {
+		print("Player `" + player.Name + "` removing.");
+	}),
+);
+
+task.delay(10, () => Collector.Disconnect("playerremoving"));
--- src/shared/easycollector.ts
+++ src/shared/easycollector.ts
@@ -1,17 +1,39 @@
-import { Debris } from "@rbxts/services";
-
 const wrapped: Record<string, RBXScriptConnection> = {};
 
 export class Collector {
-	static WrapEvent(event_unique: string, event: RBXScriptConnection): void {
-		wrapped[event_unique] = event;
+	private static SafeCheck(event_unique: string): boolean {
+		task.wait();
+		return wrapped[event_unique] !== undefined;
 	}
+
+	static GetEvent(event_unique: string): RBXScriptConnection | void {
+		task.wait();
+		if (!this.SafeCheck(event_unique)) return;
+		return wrapped[event_unique];
+	}
+
+	static WrapEvent(event_unique: string, event: RBXScriptConnection, lifetime?: number): void {
+		task.wait();
+		wrapped[event_unique] = event;
+		if (lifetime !== undefined) this.AddItem(event_unique, lifetime);
+	}
+
+	static UnwrapEvent(event_unique: string): void {
+		task.wait();
+		if (!this.SafeCheck(event_unique)) return;
+		delete wrapped[event_unique];
+	}
+
 	static Disconnect(event_unique: string): void {
+		task.wait();
+		if (!this.SafeCheck(event_unique)) return;
 		wrapped[event_unique].Disconnect();
 		delete wrapped[event_unique];
 	}
 
-	static AddItem(item: Instance, lifetime?: number): void {
-		Debris.AddItem(item, lifetime);
+	static AddItem(event_unique: string, lifetime?: number): void {
+		task.delay(lifetime ?? 0, function () {
+			Collector.Disconnect(event_unique);
+		});
 	}
 }
