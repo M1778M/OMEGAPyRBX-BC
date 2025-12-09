54a69ab1ce3ef7e1ebe4d9c10cd681688b3947d7
M1778M <m1778.pc@gmail.com>
2025-12-09 22:35:00 +0330

    Test ProviderService

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -1,3 +1,25 @@
-import { makeHello } from "shared/module";
+// import { makeHello } from "shared/module";
+// import { Players } from "@rbxts/services";
+import { ProviderService, ProviderServiceType } from "@rbxts/providerservice/";
 
-print(makeHello("main.client.ts"));
+const PS = new ProviderService();
+
+PS.createProvider("TestService", ProviderServiceType.Client);
+const TestData = {
+	x: 10,
+};
+PS.connect("TestService", "init", async () => {
+	TestData.x = 12;
+	print("Initializing");
+});
+PS.connect("TestService", "stop", async () => {
+	print("Deinitalizing");
+	TestData.x = 0;
+});
+
+print(TestData);
+PS.startAllProviders();
+print(TestData);
+task.wait(10);
+PS.stopAllProviders();
+print(TestData);
